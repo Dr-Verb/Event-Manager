31ed1757c15b7e66a0c8bf2f096edd5d337c4f04
Dr-Verb <spamodbieracz@gmail.com>
2025-08-06 02:59:18 +0200

    Generate more records to DB

--- app/src/site_parts/FooterBar.jsx
+++ app/src/site_parts/FooterBar.jsx
@@ -6,7 +6,8 @@ function FooterBar(){
         <footer className="footer"> 
             Stopka z nazwą firmy zarządzającej zgłoszeniami oraz dane kontaktowe, itp, oraz dane na temat autora strony.<br/>
             Do uzupełnienia już na samym końcu :)
-            Logo firmy użyte w projekcie <a href="http://www.freepik.com">Designed by starline / Freepik</a>
+            <p>Logo firmy użyte w projekcie <a href="http://www.freepik.com">Designed by starline / Freepik</a></p>
+            <p>Dane do bazy danych wygenerowane na stronie: <a href="https://www.mockaroo.com/"> https://www.mockaroo.com/ </a></p>
 
 
         </footer>
